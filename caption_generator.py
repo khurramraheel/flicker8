d373273e63cb7884b3112ef0800426ee4cf03cf4
Khurram Raheel <khurramraheel@yahoo.com>
2023-05-20 18:24:59 +0500

    asds

---
+++ caption_generator.py
@@ -0,0 +1,42 @@
+"""
+This file contains code for sampling caption using the model built (default: sample_model.h5)
+It will first convert all the images from the sample_images folder into a set of VGG16 features, 
+and then pass the features to the trained deep-learning model and tokenizer,
+and return the captions.
+"""
+
+import os
+import keras
+from keras.preprocessing.text import tokenizer_from_json
+import numpy as np
+import matplotlib.pyplot as plt
+from build_model.build import feature_extractions, sample_caption
+import json
+from pickle import load, dump
+from tensorflow.keras.utils import load_img
+    
+#Load tokenizer
+with open('tokenizer.json', 'r') as f:
+    tokenizer_json = json.load(f)
+tokenizer = tokenizer_from_json(tokenizer_json)
+    
+model = keras.models.load_model("./sample_model.h5") #Load model
+vocab_size = tokenizer.num_words #The number of vocabulary
+max_length = 37 #Maximum length of caption sequence
+
+#sampling
+features = feature_extractions("./sample_images")
+
+print(features)
+
+for i, filename in enumerate(features.keys()):
+    print("code chaling")
+    plt.figure(i+1)
+    caption = sample_caption(model, tokenizer, max_length, vocab_size, features[filename])
+    print(caption)
+    img = load_img("./sample_images/{fn}.jpg".format(fn=filename))
+    plt.imshow(img)
+    plt.figtext(0.5, 0.01, caption, wrap=True, horizontalalignment='center', fontsize=12)
+
+
+
